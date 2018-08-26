dd8b51f3df9f0939bf96db144ef33feac12262e0
user <user@users-Macintosh.local>
2018-08-26 12:12:20 -0400

    Angular 2 Class Assign 1 create Calculator using ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myproject';
+
+  date: Date;  // date objectfor piping
+
+  constructor() {
+    this.date = new Date();     /* create an instance of date, when the AppComponent object
+                                   is being instantiated.
+                                */
+  }
+  /* Note: a and be are operands in this calculator program
+       - iniitialized to no particular values
+       - the operands, when being used in divison are as follows:
+           a - will always be the dividen
+           b - will always the the division
+  */
+
+  a = 500;
+  b = 5;
+  result = 0;
+  operator = '+';  //default
+  divideByZeroErrMsg = ''; //default
+
+  resetOperator()
+  {
+    this.operator="";
+  }
+  resetDividend()
+  {
+    this.divideByZeroErrMsg="";
+  }
+
+  calculate(newOperator) {
+
+    document.getElementById('operatorNg').className="Center";
+
+        switch (newOperator) {
+          case '+':
+            this.result = this.a + this.b;
+            break;
+          case '-':
+            this.result = this.a - this.b;
+            break;
+          case '*':
+            this.result = this.a * this.b;
+            break;
+          case '/':
+            if(this.b.valueOf() <= 0)
+            {
+              this.divideByZeroErrMsg = 'Dividend must be > 0 ';
+              document.getElementById('divideByZero').className="CenterError";
+              break;
+            }
+            else{
+              document.getElementById('divideByZero').className="HideControl";
+              this.result = this.a / this.b;
+              break;
+            }
+          default:
+          this.operator="Sorry, only +,-,*,/ operators allowed";
+          document.getElementById('operatorNg').className="CenterError";
+
+        }
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';// whp must add for two binding ngModel
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule,CommonModule ], //< added FormsModule here for two way binding for textbox
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
